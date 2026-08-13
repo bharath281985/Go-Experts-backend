9837f7c405104053c8c51ccf8f9410089497f06f
bharath281985 <goexpertsapps@hotmail.com>
2026-08-13 11:38:56 +0530

    Fix table names in seed script

--- seed_cpanel_thumbnails.sql
+++ seed_cpanel_thumbnails.sql
@@ -1,14 +1,14 @@
-UPDATE `ResumeTemplate` SET `thumbnail` = 'https://placehold.co/420x594/ffffff/334155?text=Professional+Resume' WHERE `id` = 'f2344637-4622-478f-86f8-13bd70694675';
-UPDATE `ResumeTemplateVersion` SET `thumbnail` = 'https://placehold.co/420x594/ffffff/334155?text=Professional+Resume' WHERE `templateId` = 'f2344637-4622-478f-86f8-13bd70694675';
+UPDATE `resume_templates` SET `thumbnail` = 'https://placehold.co/420x594/ffffff/334155?text=Professional+Resume' WHERE `id` = 'f2344637-4622-478f-86f8-13bd70694675';
+UPDATE `resume_template_versions` SET `thumbnail` = 'https://placehold.co/420x594/ffffff/334155?text=Professional+Resume' WHERE `templateId` = 'f2344637-4622-478f-86f8-13bd70694675';
 
-UPDATE `ResumeTemplate` SET `thumbnail` = 'https://placehold.co/420x594/3b82f6/ffffff?text=Modern+Resume' WHERE `id` = 'e247b18b-5c49-4a12-b49f-e1202fe0c51b';
-UPDATE `ResumeTemplateVersion` SET `thumbnail` = 'https://placehold.co/420x594/3b82f6/ffffff?text=Modern+Resume' WHERE `templateId` = 'e247b18b-5c49-4a12-b49f-e1202fe0c51b';
+UPDATE `resume_templates` SET `thumbnail` = 'https://placehold.co/420x594/3b82f6/ffffff?text=Modern+Resume' WHERE `id` = 'e247b18b-5c49-4a12-b49f-e1202fe0c51b';
+UPDATE `resume_template_versions` SET `thumbnail` = 'https://placehold.co/420x594/3b82f6/ffffff?text=Modern+Resume' WHERE `templateId` = 'e247b18b-5c49-4a12-b49f-e1202fe0c51b';
 
-UPDATE `ResumeTemplate` SET `thumbnail` = 'https://placehold.co/420x594/ffffff/0f172a?text=ATS+Optimized+Resume' WHERE `id` = 'cb5be3d4-2e1e-41a0-b872-9845caf57c76';
-UPDATE `ResumeTemplateVersion` SET `thumbnail` = 'https://placehold.co/420x594/ffffff/0f172a?text=ATS+Optimized+Resume' WHERE `templateId` = 'cb5be3d4-2e1e-41a0-b872-9845caf57c76';
+UPDATE `resume_templates` SET `thumbnail` = 'https://placehold.co/420x594/ffffff/0f172a?text=ATS+Optimized+Resume' WHERE `id` = 'cb5be3d4-2e1e-41a0-b872-9845caf57c76';
+UPDATE `resume_template_versions` SET `thumbnail` = 'https://placehold.co/420x594/ffffff/0f172a?text=ATS+Optimized+Resume' WHERE `templateId` = 'cb5be3d4-2e1e-41a0-b872-9845caf57c76';
 
-UPDATE `ResumeTemplate` SET `thumbnail` = 'https://placehold.co/420x594/ec4899/ffffff?text=Creative+Resume' WHERE `id` = '912f5903-bced-4f07-8764-b661f79c232b';
-UPDATE `ResumeTemplateVersion` SET `thumbnail` = 'https://placehold.co/420x594/ec4899/ffffff?text=Creative+Resume' WHERE `templateId` = '912f5903-bced-4f07-8764-b661f79c232b';
+UPDATE `resume_templates` SET `thumbnail` = 'https://placehold.co/420x594/ec4899/ffffff?text=Creative+Resume' WHERE `id` = '912f5903-bced-4f07-8764-b661f79c232b';
+UPDATE `resume_template_versions` SET `thumbnail` = 'https://placehold.co/420x594/ec4899/ffffff?text=Creative+Resume' WHERE `templateId` = '912f5903-bced-4f07-8764-b661f79c232b';
 
-UPDATE `ResumeTemplate` SET `thumbnail` = 'https://placehold.co/420x594/0f172a/22c55e?text=Developer+Resume' WHERE `id` = 'c5700bed-c697-42a0-bcae-14623b62b057';
-UPDATE `ResumeTemplateVersion` SET `thumbnail` = 'https://placehold.co/420x594/0f172a/22c55e?text=Developer+Resume' WHERE `templateId` = 'c5700bed-c697-42a0-bcae-14623b62b057';
+UPDATE `resume_templates` SET `thumbnail` = 'https://placehold.co/420x594/0f172a/22c55e?text=Developer+Resume' WHERE `id` = 'c5700bed-c697-42a0-bcae-14623b62b057';
+UPDATE `resume_template_versions` SET `thumbnail` = 'https://placehold.co/420x594/0f172a/22c55e?text=Developer+Resume' WHERE `templateId` = 'c5700bed-c697-42a0-bcae-14623b62b057';
