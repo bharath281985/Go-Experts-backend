cf63f375adc2df22a2bd21277e9589c35f076172
bharath281985 <goexprtsapps@hotmail.com>
2026-09-19 14:30:06 +0530

    fix: import extension for prisma

--- src/controllers/public/careers.controller.ts
+++ src/controllers/public/careers.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import prisma from "../../lib/prisma";
+import prisma from "../../lib/prisma.js";
 
 // GET /api/v1/public/careers/cms
 export const getCareersCms = async (req: Request, res: Response) => {
